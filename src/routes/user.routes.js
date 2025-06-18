5e1be251a8093af8081b196229f3d616e921e37b
aviralkushwaha <aviralkushwaha2@gmail.com>
2025-06-18 13:38:43 +0530

    Refactor subscription model and API response constructor for improved clarity and consistency

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,12 +8,14 @@ import { loginUser,
         updateAccountDetails,
         updateAvatar, 
         updateCoverImage, 
+        getUserChannelProfile,
         getWatchHistory
          }from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
+
 router.route("/register").post(
     upload.fields([
         {
@@ -34,11 +36,11 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
-router.route("/current-user").get(verifyJWT,getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
-router.route("/update--avatar").patch(verifyJWT,upload.single("avatar"), updateAvatar);
+router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"), updateAvatar);
 router.route("/update-cover-image").patch(verifyJWT,upload.single("coverImage"), updateCoverImage);
 router.route("/c/:username").get(verifyJWT, getCurrentUser);
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
 router.route("/history").get(verifyJWT, getWatchHistory);
 
 export  default router
--- src/utils/ApiResponce.js
+++ src/utils/ApiResponce.js
@@ -1,5 +1,5 @@
 class ApiResponce {
-  constructor(statusCode, message, data ="Success") {
+  constructor(statusCode, data, message = "Success") {
     this.statusCode = statusCode;
     this.data = data;
     this.message = message;
